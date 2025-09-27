99bee6d09dd338d7da610a23f674d35c7c1854f1
Ha Van Nguyen <nguyen902993@gmail.com>
2025-09-27 11:26:04 +0700

    27/9 first commit

---
+++ src/test/java/com/hanguyen/hospital_management/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanguyen.hospital_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
